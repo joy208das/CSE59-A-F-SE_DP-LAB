3d74fdf7bdb31ef7ee93ec859302d80d3690e7ec
joy208das <158313870+joy208das@users.noreply.github.com>
2026-06-09 10:29:23 +0600

    Update YouDo5.java

--- Task 2/YouDo5.java
+++ Task 2/YouDo5.java
@@ -1,1 +1,20 @@
 
+class printer {
+    void printdata(string text){
+        System.out.println("text: "+text);
+    }
+    
+    void printdata(int number){
+        System.out.println("number: "+number);
+    }
+}
+
+
+public class Main{
+    
+    public static void main(String[] args){
+        printer print = new printer();
+        print.printdata("joy");
+        print.printdata(100);
+    }
+}
