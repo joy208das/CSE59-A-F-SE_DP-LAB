60e2ce69421b3aac24925965c5a19288f45b2c45
joy208das <158313870+joy208das@users.noreply.github.com>
2026-06-09 10:20:05 +0600

    Update YouDo4.java

--- Task 2/YouDo4.java
+++ Task 2/YouDo4.java
@@ -1,1 +1,19 @@
+class Sensor {
+    String name ;
+    int batterylevel;
+     
+    void charge(){
+        batterylevel = 100;
+        System.out.println("Sensor fully charged");
+    }
+    
+}
 
+public class  Main{
+    
+    public static void main(String[] args){
+        Sensor se = new Sensor();
+        se.charge();
+        System.out.println("new battery level"+ Sensor.batterylevel);
+    }
+}
