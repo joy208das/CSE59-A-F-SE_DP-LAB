4280c61c2298897a3829200c74449af4c04b16f7
joy208das <158313870+joy208das@users.noreply.github.com>
2026-06-09 10:59:37 +0600

    Update YouDo7.java

--- Task 2/YouDo7.java
+++ Task 2/YouDo7.java
@@ -1,1 +1,20 @@
 
+
+public class Main{
+    
+    public static void main(String[] args){
+        Button btn = new Button();
+        btn.onClick();
+    }
+}
+
+interface Clickable {
+    void onClick();
+}
+
+class Button implements Clickable{
+    
+    public void onClick(){
+        System.out.println("Button was clicked");
+    }
+}
