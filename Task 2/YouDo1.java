54fc009cf4d422695998823a37f4442f16c48447
joy208das <158313870+joy208das@users.noreply.github.com>
2026-06-09 09:53:55 +0600

    Update YouDo1.java

--- Task 2/YouDo1.java
+++ Task 2/YouDo1.java
@@ -1,1 +1,20 @@
+class ContestStats {
+    
+    int[] scores = {100,85,25};
+    
+    void printFirstScore(){
+        System.out.println(scores[0]);
+        
+    }
+}
 
+public class Main{
+  public static void main(String[] args){
+      
+      ContestStats stats = new ContestStats();
+      stats.printFirstScore();
+
+      
+  }   
+    
+}
