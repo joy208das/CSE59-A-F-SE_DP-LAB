648151a53a1b6b55a05fbced262f2fd19764ef07
joy208das <158313870+joy208das@users.noreply.github.com>
2026-06-16 09:53:52 +0600

    Update YouDo.java

--- Task 3/YouDo.java
+++ Task 3/YouDo.java
@@ -1,1 +1,48 @@
+//only stores data
 
+public class User {
+    private String username;
+    private String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
+//email validator 
+
+public class EmailValidator{
+    public boolean isValid(String email){
+        if(email != null && email.contains("@")){
+            return true;
+        }
+        System.out.println("Invalid email format");
+        return false;
+    }
+}
+
+//user repository interface (Dependency Inversion principle)
+public interface UserRepository{
+    void save(User user);
+}
+
+
+// dataase implementation 
+
+public class DatabaseUserRepository implements UserRepository{
+
+    @Override
+    public void save(User user){
+        System.out.println("Connecting to database");
+        System.out.println("Saving user"+user.getUsername()+"to the users table.");
+    }
+}
