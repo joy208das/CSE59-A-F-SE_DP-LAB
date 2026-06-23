0e03a8b808cef5eec81fca1ea1ee48514af45930
joy208das <158313870+joy208das@users.noreply.github.com>
2026-06-23 11:13:33 +0600

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,48 @@
 
+interface  Printer {
+void printDocument();
+    
+}
+ interface Scanner {
+void scanDocument();
+    
+} 
+
+interface FaxMachine {
+    void faxDocument();
+}
+class BasicPrinter implements Printer {
+
+    @Override
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+}
+
+class MultiFunctionCopier implements Printer, Scanner, FaxMachine {
+
+    @Override
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+
+    @Override
+    public void scanDocument() {
+        System.out.println("Scanning...");
+    }
+
+    @Override
+    public void faxDocument() {
+        System.out.println("Faxing...");
+    }
+}
+public class Main {
+    public static void main(String[] args){
+        BasicPrinter printer = new BasicPrinter();
+        printer.printDocument();
+        MultiFunctionCopier copier = new MultiFunctionCopier();
+        copier.printDocument();
+        copier.scanDocument();
+        copier.faxDocument();
+    }
+}
