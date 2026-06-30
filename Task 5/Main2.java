a13d995dc82399bab785c12bb4683447efa0426a
joy208das <158313870+joy208das@users.noreply.github.com>
2026-06-30 10:18:30 +0600

    Update Main2.java

--- Task 5/Main2.java
+++ Task 5/Main2.java
@@ -1,1 +1,46 @@
 
+
+interface MessageSender {
+    void sendMessage(String message);
+}
+
+
+class EmailSender implements MessageSender {
+
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+
+class SMSSender implements MessageSender {
+
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+class NotificationService{
+    private MessageSender messageSender;
+
+    public NotificationService(MessageSender messageSender){
+        this.messageSender = messageSender;
+    }
+    public void alerUser(String msg){
+        messageSender.sendMessage(msg);
+    }
+}
+
+public class Main{
+    public static void main(String[] args){
+        MessageSender email = new EmailSender();
+        NotificationService emailNotification = new NotificationService(email);
+        emailNotification.alerUser("your assignment is due tomorrow.");
+
+        MessageSender sms = new SMSSender();
+        NotificationService smsNotification = new NotificationService(sms);
+        smsNotification.alerUser("your otp is 123456");
+    }
+}
