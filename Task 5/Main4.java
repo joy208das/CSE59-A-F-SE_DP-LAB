24e4b64aff656caef4cf1a6a6183102bb4a53bec
joy208das <158313870+joy208das@users.noreply.github.com>
2026-06-30 10:38:35 +0600

    Update Main4.java

--- Task 5/Main4.java
+++ Task 5/Main4.java
@@ -1,1 +1,42 @@
 
+
+ interface OptimizableModel {
+void quantize();
+    
+}
+class ResNet18 implements OptimizableModel {
+
+    @Override
+    public void quantize() {
+        System.out.println("Applying int8 quantization for ResNet-18.");
+    }
+}
+
+
+class MobileNet implements OptimizableModel {
+
+    @Override
+    public void quantize() {
+        System.out.println("Applying dynamic quantization for MobileNet.");
+    }
+}
+
+class InferenceEngine {
+
+    public void optimize(OptimizableModel model) {
+        model.quantize();
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+
+        InferenceEngine engine = new InferenceEngine();
+
+        OptimizableModel resent = new ResNet18();
+        OptimizableModel mobile = new MobileNet();
+
+        engine.optimize(resent);
+        engine.optimize(mobile);
+    }
+}
