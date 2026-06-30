5a86c4eb5e934e9a0a1a7a4103cfcd64b14cd400
joy208das <158313870+joy208das@users.noreply.github.com>
2026-06-30 10:55:54 +0600

    Update Main6.java

--- Task 5/Main6.java
+++ Task 5/Main6.java
@@ -1,1 +1,54 @@
 
+interface HttpService {
+    void handleHttpRequest();
+}
+
+interface DatabaseManager {
+    void executeSqlStatement();
+}
+
+interface ContainerOps {
+    void restartDockerContainer();
+}
+
+
+class WebController implements HttpService {
+
+    @Override
+    public void handleHttpRequest() {
+        System.out.println("Routing traffic to endpoint.");
+    }
+}
+
+
+class PostgresDatabase implements DatabaseManager {
+
+    @Override
+    public void executeSqlStatement() {
+        System.out.println("Executing SQL query on PostgreSQL.");
+    }
+}
+
+
+class DockerService implements ContainerOps {
+
+    @Override
+    public void restartDockerContainer() {
+        System.out.println("Restarting Docker container.");
+    }
+}
+
+
+public class Main{
+    public static void main(String[] args){
+        HttpService controller = new WebController();
+        controller.handleHttpRequest();
+
+        DatabaseManager db = new PostgresDatabase();
+        db.executeSqlStatement();
+
+        ContainerOps docker = new DockerService();
+        docker.restartDockerContainer();
+        
+    }
+}
